78279c642439a2adb05ee042e47faf68818820e1
Juan Miguel Ruiz Pazos <juanmiguelruizpazos@gmail.com>
2025-01-29 21:02:15 +0100

    fix: update spinner color to black

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -3,7 +3,7 @@ bouncy.register();
 
 const Spinner = (): JSX.Element => (
   <div className="w-screen h-screen flex justify-center items-center bg-amber-50">
-    <l-bouncy size="60" speed="1.75" color="#73D4FF"></l-bouncy>
+    <l-bouncy size="60" speed="1.75" color="black"></l-bouncy>
   </div>
 );
 
