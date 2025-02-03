a545b960316a1a68e9d7d0d0b63e22aae27a475f
Juan Miguel Ruiz Pazos <juanmiguelruizpazos@gmail.com>
2025-02-03 21:11:38 +0100

    refactor: adjust padding and max-width for improved layout responsiveness

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ import Header from '../Header';
 const Layout = (): JSX.Element => (
   <div className="bg-amber-50 min-h-screen">
     <Header />
-    <div className="w-full max-w-8/10 p-8 mx-auto">
+    <div className="w-full p-4 md:max-w-8/10 md:p-8 mx-auto">
       <Outlet />
     </div>
   </div>
